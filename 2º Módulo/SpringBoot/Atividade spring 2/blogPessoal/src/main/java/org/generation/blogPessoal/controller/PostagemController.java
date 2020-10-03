89610f09abc8705a01b37449c63352200217c554
LssLucas <53626965+LssLucas@users.noreply.github.com>
2020-10-02 21:45:01 -0300

    Atividade 3

--- 2º Módulo/SpringBoot/Atividade spring 2/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ 2º Módulo/SpringBoot/Atividade spring 2/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -2,15 +2,18 @@ package org.generation.blogPessoal.controller;
 
 import java.util.List;
 
-import javax.xml.ws.Response;
-
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -33,9 +36,23 @@ public class PostagemController {
 				.map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/titulo/{titulo}")
+	@GetMapping("/titulo/{titulo}")//caminho URI
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	@PostMapping
+	public ResponseEntity <Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity <Postagem> put (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")//para passar um parametro é constrida essa estrutura
+	public void delete (@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
