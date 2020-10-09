0f287b054401dc32c26b276ffe2e7e4e06afae70
unknown <jefferson.russo@Outlook.com>
2020-10-09 12:17:04 -0300

    Postagem, model repository e controller

--- vaiDarBomPlataformaDeEstudos/src/main/java/com/vaiDarBom/vaiDarBomPlataformaDeEstudos/model/Postagem.java
+++ vaiDarBomPlataformaDeEstudos/src/main/java/com/vaiDarBom/vaiDarBomPlataformaDeEstudos/model/Postagem.java
@@ -1,48 +1,54 @@
 package com.vaiDarBom.vaiDarBomPlataformaDeEstudos.model;
 
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.blogPessoal.MeuBlog.model.Tema;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name="model")
-public class Model {
+@Table(name = "Postagem")
+public class Postagem {
+
 	/*
-	 id
-	 assunto
-	 nivelEnsino
-	 disciplinaMateria
-	 ativo
-	*/	
-	
-	//Criando coluna/atributo id
+	 * ID PK Assunto String NivelEnsino String DisciplinaMateria String ATIVO BOOL
+	 * id_Tema ( Tema OBJ ) FK
+	 */
+
+	// Criando coluna/atributo id
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
-	//Criando coluna/atributo 'assunto'
+
+	// Criando coluna/atributo 'assunto'
 	@NotNull
-	@Size(min=1,max=100)
+	@Size(min = 1, max = 100)
 	private String assunto;
-	
-	//Criando coluna/atributo 'nivelDeEnsino'
+
+	// Criando coluna/atributo 'nivelDeEnsino'
 	@NotNull
-	@Size(min=1,max=100)
+	@Size(min = 1, max = 100)
 	private String nivelEnsino;
-	
-	//Criando coluna/atributo 'disciplinaMateria'
+
+	// Criando coluna/atributo 'disciplinaMateria'
 	@NotNull
-	@Size(min=1,max=100)
+	@Size(min = 1, max = 100)
 	private String disciplinaMateria;
-	
-	//Criando coluna/atributo 'ativo'
+
+	// Criando coluna/atributo 'ativo'
 	@NotNull
 	private boolean ativo;
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
