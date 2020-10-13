a9bc4d67ddb8bf2d4cf4816eddcaacac2ca5be4c
LssLucas <53626965+LssLucas@users.noreply.github.com>
2020-10-13 16:12:25 -0300

    Criação Interface UsuarioRepository Classe UsuarioController

---
+++ vaiDarBomPlataformaDeEstudos/src/main/java/com/vaiDarBom/vaiDarBomPlataformaDeEstudos/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.vaiDarBom.vaiDarBomPlataformaDeEstudos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vaiDarBom.vaiDarBomPlataformaDeEstudos.model.Usuario;
+
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
+	public List<Usuario> findAllByNomeContainingIgnoreCase (String nome);
+	//fazer com email
+}
